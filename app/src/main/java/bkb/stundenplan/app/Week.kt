5d664e6d2302eca05b0495a7d1239569924750ee
Paul Brandt <pa.brandt@gmx.de>
2025-01-09 23:47:52 +0100

    5.1 Jsoup seemingly completely working

--- app/src/main/java/bkb/stundenplan/app/Week.kt
+++ app/src/main/java/bkb/stundenplan/app/Week.kt
@@ -1,5 +1,7 @@
 package bkb.stundenplan.app
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.ui.graphics.Color
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
@@ -15,89 +17,136 @@ data class Text(
  *in html wird dieses attribut rowspan genannt
  */
 data class Subject(
-    val multiplier: Int, val content:Element
+    val multiplier: Int, val content: Element
 )
 
 data class Day(
-    val date: LocalDate?, val subjects: List<Subject>?
+    var date: LocalDate?, var subjects: MutableList<Subject>?
 )
-class Week(val monday: Day, val tuesday: Day, val wednesday: Day, val thursday: Day, val friday: Day)
-{
-    fun getToday(): Day
-    {return Day(null,null)}
+
+class Week(
+    var monday: Day?, var tuesday: Day?, var wednesday: Day?, var thursday: Day?, var friday: Day?
+) {
+
+    fun asList(): List<Day?> = listOf(monday, tuesday, wednesday, thursday, friday)
+    fun asMap(): Map<LocalDate?,Day?> = mapOf(monday?.date to monday,tuesday?.date to tuesday,wednesday?.date to wednesday,thursday?.date to thursday,friday?.date to friday)
+
+    fun setDay(index: Int, day: Day) {
+        when (index)
+        {
+            1 -> monday = day
+            2 -> tuesday = day
+            3 -> wednesday = day
+            4 -> thursday = day
+            5 -> friday = day
+        }
+    }
+    fun getDay(index: Int):Day? {
+        return when (index)
+        {
+            1 -> monday
+            2 -> tuesday
+            3 -> wednesday
+            4 -> thursday
+            5 -> friday
+            else -> throw IndexOutOfBoundsException("Index muss zwischen 1 und 5 sein")
+        }
+    }
 
 
-    fun getTomorrow(): Day
-    {return Day(null,null)}
 
-    fun getWeek(): List<Day>?
-    {return null}
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getToday(): Day? {
+        val today = LocalDate.now()
+         this.asMap().forEach(){if(it.key == today) return it.value}
+    return null
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getNextDayOrMonday(): Day? {
+        val today = LocalDate.now()
+        this.asMap().forEach(){if(it.key == today.plusDays(1)) return it.value}
+        this.asMap().forEach(){if(it.key == today.plusDays(2)) return it.value}
+        return null
+    }
+ /*
+    fun getWeek(): List<Day>? {
+        return null
+    }*/
 }
 
 /**
  * Return the Week as `List<Day>?`
  */
- fun Document.getWeek(): List<Day>?
- {
-     data class weekStringsClass(var mo:String = "",var tu:String = "",var we:String = "",var th:String = "",var f:String = "")
-     fun getDates(dates:Element):weekStringsClass
-     {
-         var mo = "error";var tu = "error";var we = "error";var th = "error";var f = "error"
-         var count = 0
-         dates.select("td > table > tbody > tr > td > font").forEach {
-            when(count)
-            {
-                0->mo = it.text()
-                1->tu = it.text()
-                2->we = it.text()
-                3->th = it.text()
-                4->f = it.text()
+fun Document.getWeek(): Week? {
+    data class weekStringsClass(
+        var mo: String = "error",
+        var tu: String = "error",
+        var we: String = "error",
+        var th: String = "error",
+        var fr: String = "error"
+    )
+
+    fun getDates(dates: Element): weekStringsClass {
+        var weekStrings = weekStringsClass()
+
+        var count = 0
+        dates.select("td > table > tbody > tr > td > font").forEach {
+            when (count) {
+                0 -> weekStrings.mo = it.text()
+                1 -> weekStrings.tu = it.text()
+                2 -> weekStrings.we = it.text()
+                3 -> weekStrings.th = it.text()
+                4 -> weekStrings.fr = it.text()
             }
             count++
         }
-    return weekStringsClass(mo,tu,we,th,f)
-     }
+        return weekStrings
+    }
 
 
-     var actualRowCounter = 0 //max 20 one cell takes 2
-     var cDayOne = 0
-     var cDayTwo = 0
-     var cDayThree = 0
-     var cDayFour = 0
-     var cDayFive = 0
+    var rowspanCounter = 0 //max 20 one cell takes 2
+    var cDayOne = 0
+    var cDayTwo = 0
+    var cDayThree = 0
+    var cDayFour = 0
+    var cDayFive = 0
 
-     var listWeek: MutableList<Day>? = null
-     var weekStrings = weekStringsClass()
+    var week: Week = Week()
+    var weekStrings = weekStringsClass()
 
-     val rows = this.select("table")?.get(0)?.selectFirst("table > tbody")?.select("> tr")
+    val rows = this.select("table")?.get(0)?.selectFirst("table > tbody")?.select("> tr")
 
-         rows?.forEach {counterRow->
+    rows?.forEach { counterRow ->
 
-        if(counterRow.childrenSize() != 0)
-        {
-            if(counterRow == rows.first())
-            {
+        if (counterRow.childrenSize() != 0) {
+            if (counterRow == rows.first()) {
                 weekStrings = getDates(counterRow)
             }
-            else
-            {
+            else {
                 val cells = counterRow.select(">td")
                 var dayCounter = 1//diese Variable steht für Montag bis freitag
-                cells?.forEach {counterCell->
+                cells?.forEach { counterCell ->
 
-                    if(counterCell != cells.first())
-                    {
+                    if (counterCell != cells.first()) {
                         val multiplier = counterCell.attributes()["rowspan"]?.toInt() ?: 0
-                        when(dayCounter) {
-                            1 -> cDayOne += multiplier
-                            2 -> cDayTwo += multiplier
-                            3 -> cDayThree += multiplier
-                            4 -> cDayFour += multiplier
-                            5 -> cDayFive += multiplier
+                        var currentRowspan = when (dayCounter) {
+                            1 -> {cDayOne += multiplier
+                                cDayOne}
+                            2 -> {cDayTwo += multiplier
+                                cDayTwo}
+                            3 -> {cDayThree += multiplier
+                                cDayThree}
+                            4 -> {cDayFour += multiplier
+                                cDayFour}
+                            5 -> {cDayFive += multiplier
+                             cDayFive}
+                            else -> 20
                         }
+                        var oldRowspan = currentRowspan - multiplier
 
-                        if()
+                        if (rowspanCounter > oldRowspan)// der rowspan der sein sollte ist  größer als der der momentan ist, deswegen ist noch platz
                         {
+                            week.setDay()
 
                         }
 
@@ -107,12 +156,11 @@ class Week(val monday: Day, val tuesday: Day, val wednesday: Day, val thursday: 
                 }
 
 
-                actualRowCounter++
+                rowspanCounter += 2
             }
-
 
         }
 
     }
-     return null
- }
+    return week
+}
